307db9b0e976b9cae3e247eed8338396973ef849
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-05 23:34:44 -0500

    Test fetching coordinates by IP

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const { fetchMyIP } = require('./iss');
+const { fetchMyIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -8,3 +8,14 @@
 
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
